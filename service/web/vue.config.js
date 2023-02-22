3f62dc04283c0a64e1fc3e29ef920fda853899bc
石松 <rgkjhshi@gmail.com>
2023-02-22 19:36:51 +0800

    Update vue.config.js

--- service/web/vue.config.js
+++ service/web/vue.config.js
@@ -42,13 +42,14 @@ module.exports = {
       // change xxx-api/login => mock/login
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:${port}/`,
+        target: `http://localhost:8080/`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
         }
       }
     },
+    // after: require('./mock/mock-server.js')
     before: require('./mock/mock-server.js')
   },
   configureWebpack: {
