97596fced8a0aff09d34959e094e6a3d620e1652
石松 <rgkjhshi@gmail.com>
2023-03-30 23:21:39 +0800

    Update infoList.vue

--- service/web/src/views/fei-biao/infoList.vue
+++ service/web/src/views/fei-biao/infoList.vue
@@ -76,7 +76,7 @@ import clip from '@/utils/clipboard' // use clipboard directly
 import clipboard from '@/directive/clipboard/index.js' // use clipboard by v-directive
 
 export default {
-  name: 'ClipboardDemo',
+  name: 'InfoList',
   directives: {
     clipboard
   },
